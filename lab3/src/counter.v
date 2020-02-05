6bc5a1e811739831865491d1d2907d4100e3c601
Tan Nguyen <tan.nqd@gmail.com>
2020-02-05 15:06:47 -0800

    added missing file

---
+++ lab3/src/counter.v
@@ -0,0 +1,15 @@
+`include "../../lib/EECS151.v"
+
+module counter #(
+    parameter N = 4,
+    parameter RATE_HZ = 1
+) (
+    input clk,
+    input rst_counter,
+    input [N-1:0] rst_counter_val,
+    output [N-1:0] counter_output
+);
+
+    //TODO : Your code
+
+endmodule
