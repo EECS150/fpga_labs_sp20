f94ee231f51dd5e2f8e1da82165fe982b83bc78f
Tan Nguyen <tan.nqd@gmail.com>
2020-02-15 01:38:27 -0800

    [eecs151 lib] init regs and mems properly

--- lib/EECS151.v
+++ lib/EECS151.v
@@ -29,7 +29,7 @@ module REGISTER(q, d, clk);
    output reg [N-1:0] q;
    input [N-1:0]      d;
    input 	     clk;
-   initial q = {N{1'b0}};
+   initial q <= {N{1'b0}};
    always @(posedge clk)
     q <= d;
 endmodule // REGISTER
@@ -52,7 +52,7 @@ module REGISTER_R(q, d, rst, clk);
    output reg [N-1:0] q;
    input [N-1:0]      d;
    input 	      rst, clk;
-   initial q = {N{1'b0}};
+   initial q = INIT;
    always @(posedge clk)
      if (rst) q <= INIT;
      else q <= d;
@@ -66,7 +66,7 @@ module REGISTER_R_CE(q, d, rst, ce, clk);
    output reg [N-1:0] q;
    input [N-1:0]      d;
    input 	      rst, ce, clk;
-   initial q = {N{1'b0}};
+   initial q = INIT;
    always @(posedge clk)
      if (rst) q <= INIT;
      else if (ce) q <= d;
@@ -90,12 +90,18 @@ module ASYNC_ROM(q, addr, clk);
     output [DWIDTH-1:0] q;
     reg [DWIDTH-1:0] mem [DEPTH-1:0];
 
+    integer i;
     initial begin
         if (MEM_INIT_HEX_FILE != "") begin
 	          $readmemh(MEM_INIT_HEX_FILE, mem);
         end
         else if (MEM_INIT_BIN_FILE != "") begin
 	          $readmemb(MEM_INIT_BIN_FILE, mem);
+        end
+        else begin
+            for (i = 0; i < DEPTH; i = i + 1) begin
+                mem[i] = 0;
+            end
         end
     end
 
@@ -115,12 +121,18 @@ module ASYNC_RAM(q, d, addr, we, clk);
     output [DWIDTH-1:0] q;
     reg [DWIDTH-1:0] mem [DEPTH-1:0];
 
+    integer i;
     initial begin
         if (MEM_INIT_HEX_FILE != "") begin
 	          $readmemh(MEM_INIT_HEX_FILE, mem);
         end
         else if (MEM_INIT_BIN_FILE != "") begin
 	          $readmemb(MEM_INIT_BIN_FILE, mem);
+        end
+        else begin
+            for (i = 0; i < DEPTH; i = i + 1) begin
+                mem[i] = 0;
+            end
         end
     end
 
@@ -148,12 +160,18 @@ module SYNC_ROM(q, addr, clk);
     output [DWIDTH-1:0] q;
     reg [DWIDTH-1:0] mem [DEPTH-1:0];
 
+    integer i;
     initial begin
         if (MEM_INIT_HEX_FILE != "") begin
 	          $readmemh(MEM_INIT_HEX_FILE, mem);
         end
         else if (MEM_INIT_BIN_FILE != "") begin
 	          $readmemb(MEM_INIT_BIN_FILE, mem);
+        end
+        else begin
+            for (i = 0; i < DEPTH; i = i + 1) begin
+                mem[i] = 0;
+            end
         end
     end
 
@@ -178,12 +196,18 @@ module SYNC_RAM(q, d, addr, we, clk);
     output [DWIDTH-1:0] q;
     reg [DWIDTH-1:0] mem [DEPTH-1:0];
 
+    integer i;
     initial begin
         if (MEM_INIT_HEX_FILE != "") begin
 	          $readmemh(MEM_INIT_HEX_FILE, mem);
         end
         else if (MEM_INIT_BIN_FILE != "") begin
 	          $readmemb(MEM_INIT_BIN_FILE, mem);
+        end
+        else begin
+            for (i = 0; i < DEPTH; i = i + 1) begin
+                mem[i] = 0;
+            end
         end
     end
 
@@ -195,5 +219,56 @@ module SYNC_RAM(q, d, addr, we, clk);
     end
 
     assign q = read_reg_val;
+endmodule // SYNC_RAM
+
+// Dual-ported RAM with synchronous read
+module SYNC_RAM_DP(q0, d0, addr0, we0, q1, d1, addr1, we1, clk);
+    parameter DWIDTH = 8;               // Data width
+    parameter AWIDTH = 8;               // Address width
+    parameter DEPTH = 256;              // Memory depth
+    parameter MEM_INIT_HEX_FILE = "";
+    parameter MEM_INIT_BIN_FILE = "";
+    input clk;
+    input [DWIDTH-1:0] d0;               // Data input
+    input [AWIDTH-1:0] addr0;            // Address input
+    input 	           we0;
+    output [DWIDTH-1:0] q0;
+
+    input [DWIDTH-1:0] d1;               // Data input
+    input [AWIDTH-1:0] addr1;            // Address input
+    input 	           we1;
+    output [DWIDTH-1:0] q1;
+
+    reg [DWIDTH-1:0] mem [DEPTH-1:0];
+
+    integer i;
+    initial begin
+        if (MEM_INIT_HEX_FILE != "") begin
+	          $readmemh(MEM_INIT_HEX_FILE, mem);
+        end
+        else if (MEM_INIT_BIN_FILE != "") begin
+	          $readmemb(MEM_INIT_BIN_FILE, mem);
+        end
+        else begin
+            for (i = 0; i < DEPTH; i = i + 1) begin
+                mem[i] = 0;
+            end
+        end
+    end
+
+    reg [DWIDTH-1:0] read0_reg_val;
+    reg [DWIDTH-1:0] read1_reg_val;
+    always @(posedge clk) begin
+        if (we0)
+            mem[addr0] <= d0;
+        if (we1)
+            mem[addr1] <= d1;
+        read0_reg_val <= mem[addr0];
+        read1_reg_val <= mem[addr1];
+    end
+
+    assign q0 = read0_reg_val;
+    assign q1 = read1_reg_val;
+
 endmodule // SYNC_RAM
 
